c438d81e9dfdc5e636a83f073e1bb939a3adcba8
Dimo <dimo.dimow@tu-dortmund.de>
2026-05-04 06:44:53 +0200

    Revisit exercises: improved implementations

--- 04_functions/01_lab/01_absolute_values.py
+++ 04_functions/01_lab/01_absolute_values.py
@@ -1,7 +1,16 @@
-def sequence_to_absolute_list(sequence):
-    for i in range(len(sequence)):
-        sequence[i] = abs(float(sequence[i]))
-    return sequence
+numbers = input()
+
+def abs_values(nums: str)-> list[float]:
+    result = [abs(float(el)) for el in nums.split(" ")]
+    return result
+
+print(abs_values(numbers))
 
 
-print(sequence_to_absolute_list(input().split()))
+# def sequence_to_absolute_list(sequence):
+#     for i in range(len(sequence)):
+#         sequence[i] = abs(float(sequence[i]))
+#     return sequence
+
+
+# print(sequence_to_absolute_list(input().split()))
--- 04_functions/01_lab/05_orders.py
+++ 04_functions/01_lab/05_orders.py
@@ -1,3 +1,23 @@
+coffee_price = 1.5
+water_price = 1.0
+coke_price = 1.4
+snacks_price = 2.0
+
+def calc_price(item: str, quantity: int)-> float:
+    if item == "coffee":
+        return coffee_price * quantity
+    elif item == "water":
+        return water_price * quantity
+    elif item == "coke":
+        return coke_price * quantity
+    elif item == "snacks":
+        return snacks_price * quantity
+
+order = input()
+n = int(input())
+
+print(f"{calc_price(order, n):.2f}")
+
 '''
 TASK:
 Write a function which calculates the total price of an order and returns it. The function should receive one of the
